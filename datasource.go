61751bc3203a89b588515681ba03c9453d9eab50
scratch.net@gmail.com <7b34588f94bab4f35aa7ee7892da960fb8bab72d>
2018-01-07 23:34:29 -0800

    Added copyright

--- datasource.go
+++ datasource.go
@@ -1,3 +1,39 @@
+/*
+ * Copyright (C) 2015-2018 Virgil Security Inc.
+ *
+ * Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   (1) Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ *   (2) Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ *
+ *   (3) Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package virgil_crypto_go
 
 import (
@@ -5,17 +41,16 @@ import (
 )
 
 type dataSource struct {
-	reader io.Reader
+	reader  io.Reader
 	hasData bool
-	buf []byte
-
+	buf     []byte
 }
 
-func NewDataSource(reader io.Reader) *dataSource{
+func NewDataSource(reader io.Reader) *dataSource {
 	return &dataSource{
-		hasData:true,
-		buf: make([]byte, 4096),
-		reader:reader,
+		hasData: true,
+		buf:     make([]byte, 4096),
+		reader:  reader,
 	}
 }
 
@@ -24,13 +59,12 @@ func (d *dataSource) HasData() (_swig_ret bool) {
 }
 
 func (d *dataSource) Read() (_swig_ret VirgilByteArray) {
-	if(! d.hasData){
+	if !d.hasData {
 		return
 	}
 	read, err := d.reader.Read(d.buf)
-	if(read < len(d.buf) || err != nil){
+	if read < len(d.buf) || err != nil {
 		d.hasData = false
 	}
 	return ToVirgilByteArray(d.buf[:read])
 }
-
